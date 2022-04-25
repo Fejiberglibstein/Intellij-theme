bd9f67567761fc174ddd8170182a0a08b2a828d4
GitHub Action <action@github.com>
2022-04-25 03:38:38 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/fejiberglibstein/intellijtheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fejiberglibstein.intellijtheme.services
+
+import com.github.fejiberglibstein.intellijtheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
